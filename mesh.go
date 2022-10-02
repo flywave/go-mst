d43f0947e347db2554d326bb5c630aa62d880e2e
hj <hawking2215@gmail.com>
2022-10-02 20:14:01 +0800

    update

--- mesh.go
+++ mesh.go
@@ -77,6 +77,9 @@ const (
 
 type MeshMaterial interface {
 	HasTexture() bool
+	GetTexture() *Texture
+	GetColor() [3]byte
+	GetEmissive() [3]byte
 }
 
 type Texture struct {
@@ -99,6 +102,18 @@ func (m *BaseMaterial) HasTexture() bool {
 	return false
 }
 
+func (m *BaseMaterial) GetEmissive() [3]byte {
+	return [3]byte{0, 0, 0}
+}
+
+func (m *BaseMaterial) GetTexture() *Texture {
+	return nil
+}
+
+func (m *BaseMaterial) GetColor() [3]byte {
+	return m.Color
+}
+
 type TextureMaterial struct {
 	BaseMaterial
 	Texture *Texture `json:"texture,omitempty"`
@@ -109,9 +124,13 @@ func (m *TextureMaterial) HasTexture() bool {
 	return m.Texture != nil
 }
 
+func (m *TextureMaterial) GetTexture() *Texture {
+	return m.Texture
+}
+
 type PbrMaterial struct {
 	TextureMaterial
-	Emissive            [4]byte `json:"emissive"`
+	Emissive            [3]byte `json:"emissive"`
 	Metallic            float32 `json:"metallic"`
 	Roughness           float32 `json:"roughness"`
 	Reflectance         float32 `json:"reflectance"`
@@ -127,6 +146,10 @@ type PbrMaterial struct {
 	SubSurfaceColor     [3]byte `json:"subSurfaceColor"` // subsurface or cloth
 }
 
+func (m *PbrMaterial) GetEmissive() [3]byte {
+	return m.Emissive
+}
+
 type LambertMaterial struct {
 	TextureMaterial
 	Ambient  [3]byte `json:"ambient"`
@@ -139,6 +162,10 @@ type PhongMaterial struct {
 	Specular    [3]byte `json:"specular"`
 	Shininess   float32 `json:"shininess"`
 	Specularity float32 `json:"specularity"`
+}
+
+func (m *LambertMaterial) GetEmissive() [3]byte {
+	return m.Emissive
 }
 
 type Face struct {
